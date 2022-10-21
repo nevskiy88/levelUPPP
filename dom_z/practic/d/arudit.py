38596349b4fc39fd14318711eb05be81aadd28e1
nevskiy88 <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjx6ny4ClbHGs0g09ImbOkAWSPKlzqsv0WmCYR2uODA pavel_kovalev88@mail.ru>
2022-10-21 20:02:08 +0300

    ok

--- dom_z/practic/d/arudit.py
+++ dom_z/practic/d/arudit.py
@@ -1,12 +1,13 @@
 my_dict ={
-1:  [A, E, I, L, N, O, R, S, T, U],
-2:	[D, G],
-4:	[F, H, V, W , Y],
-3:	[B, C, M, P],
-5:	[K],
-8:	[J, X],
-10: [Q, Z]
+1:  ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"],
+2:	["D", "G"],
+4:	["F", "H", "V", "W", "Y"],
+3:	["B", "C", "M", "P"],
+5:	["K"],
+8:	["J", "X"],
+10: ["Q", "Z"]
 }
+
 slovo = input("введите слово   ").upper()
 my_list = []
 for i in slovo:
@@ -14,6 +15,6 @@ for i in slovo:
 total = 0
 for j in my_list:
     for k,v in my_dict.items():
-        if v in j:
+        if j in v:
             total +=k
 print(total)
