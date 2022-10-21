9ae9b677b948296e584f4262f148f1a173503b4f
nevskiy88 <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjx6ny4ClbHGs0g09ImbOkAWSPKlzqsv0WmCYR2uODA pavel_kovalev88@mail.ru>
2022-10-21 19:57:55 +0300

    zip

---
+++ dom_z/practic/d/arudit.py
@@ -0,0 +1,19 @@
+my_dict ={
+1:  [A, E, I, L, N, O, R, S, T, U],
+2:	[D, G],
+4:	[F, H, V, W , Y],
+3:	[B, C, M, P],
+5:	[K],
+8:	[J, X],
+10: [Q, Z]
+}
+slovo = input("введите слово   ").upper()
+my_list = []
+for i in slovo:
+    my_list.append(i)
+total = 0
+for j in my_list:
+    for k,v in my_dict.items():
+        if v in j:
+            total +=k
+print(total)
