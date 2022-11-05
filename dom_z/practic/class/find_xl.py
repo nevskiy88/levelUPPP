2034fcd01d6d47c8e95beb6cf9293cda8e6f5c4d
nevskiy88 <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjx6ny4ClbHGs0g09ImbOkAWSPKlzqsv0WmCYR2uODA pavel_kovalev88@mail.ru>
2022-11-05 15:29:52 +0300

    получилось вот так находит что надо, ЕЩЕ НЕОБХОДИМО ЗАМЕНИТЬ "None", сделать возможность добавить коментарии, добавить новую строку с пробой

--- dom_z/practic/class/find_xl.py
+++ dom_z/practic/class/find_xl.py
@@ -1,12 +1,11 @@
 import openpyxl
 file = openpyxl.reader.excel.load_workbook(filename = 'teams.xlsx')
-# print(file.sheetnames)
-file.active = 0
+sheet = file['список1']
 
-sheet = file.active
+
+#sheet = file.act
 # print(sheet['A3'].value)
-proba_number = input("введите название пробы: ").upper()
+proba_number = input("введите название пробы: ")
 for i in range(2, 312):
-    for proba_number in sheet:
-        if proba_number == i:
+        if proba_number in sheet['A' +str(i)].value:
             print(f" Название: {sheet['A'+str(i)].value}, Интервал: {sheet['B'+str(i)].value} Тара:{sheet['C'+str(i)].value} Примечание: {sheet['D'+str(i)].value} Где: {sheet['E'+str(i)].value}" )
